1ac00b9102c2c4438040a41d540e53cc06f92e33
dineshwar19 <101964219+dineshwar19@users.noreply.github.com>
2023-07-25 17:33:40 +0530

    Binary search examples

---
+++ Binary Search/src/FirstandLastPostionOfAnArray.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class FirstandLastPostionOfAnArray {
+    public static void main(String[] args) {
+        int[]arr = {5, 7, 7, 8, 8, 10};
+        int target = 7;
+        System.out.println(Arrays.toString(searchRange(arr , target)));
+    }
+    static int[] searchRange(int[] nums, int target) {
+        int[]ans = {-1,-1};
+
+        ans[0] = search(nums , target ,true);
+        if(ans[0]!=-1){
+            ans[1] = search(nums,target,false);
+        }
+        return ans;
+    }
+    static int search(int[]arr, int target,boolean startIndex){
+
+        int start = 0;
+        int end = arr.length-1;
+        int num = -1;
+
+        while(start<=end){
+            int mid = start + (end - start ) / 2;
+
+            if(target<arr[mid]){
+                end = mid - 1;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                num = mid;
+                if(startIndex){
+                    end = mid -1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return num;
+    }
+}
