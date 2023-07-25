aaf5bf044b6eede7ff4cccb317ca94e3b804e8c5
dineshwar19 <101964219+dineshwar19@users.noreply.github.com>
2023-07-25 17:35:08 +0530

    modified examples

--- Binary Search/src/Main.java
+++ Binary Search/src/Main.java
@@ -1,24 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[]arr = {3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+        int target = 10;
+        System.out.println(binarySearch(arr,target));
     }
-    static char binarySearch(char[]letters,char target){
+    static int binarySearch(int[]arr,int target){
 
         int start=0;
-        int end = letters.length-1;
+        int end = arr.length-1;
 
 
         while (start<=end){
             int mid = start + (end - start)/2;
 
-            if (target<letters[mid]) {
+            if (target<arr[mid]) {
                 end = mid - 1;
-            } else if (target>letters[mid]) {
+            } else if (target>arr[mid]) {
                 start = mid + 1;
             }else{
                 return mid;
             }
         }
-        return letters[start%letters.length];
+        return -1;
     }
 }
